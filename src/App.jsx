72654f3b4c87a7a7c5fa753c373cdbb659de7577
Sherrrrryyy <Shaheerkhan090909@gmail.com>
2024-12-13 17:23:51 +0500

    useEffect

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  //   const [count, setCount] = useState(0)
+  //   const [count1, setCount1] = useState(0)
+
+  // useEffect(()=>{
+  // console.log("counter=>", count1);
+
+  // },[count1])
+
+
+  return (
+    <>
+
+      {/* <h1>count 1 {count}</h1>
+<h1>count 2{count1}</h1>
+<button onClick={()=> setCount(count + 1)}>count</button>
+<button onClick={()=> setCount1(count1 - 1)}>count1</button> */}
+
+      <div>
+        <h1>Shopping List</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
