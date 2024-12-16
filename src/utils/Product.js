bd1c948c80bd81e958bb81000495ff854d66ae7e
Sherrrrryyy <Shaheerkhan090909@gmail.com>
2024-12-16 21:45:17 +0500

    ''

--- src/utils/Product.js
+++ src/utils/Product.js
@@ -2,8 +2,7 @@ import React from 'react'
 
 export async function getProducts() {
     try {
-        const products = await fetch
-            ('https://api.escuelajs.co/api/v1/products').then((res) => res.json())
+        const products = await fetch('https://api.escuelajs.co/api/v1/products').then((res) => res.json())
         return products;
     } catch (error) {
         throw Error("Something went wrong 404")
